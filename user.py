e77d40e592543f4082f2aa0cef98fc553554f5eb
layersony <sammaingi5@gmail.com>
2021-05-28 12:17:24 +0300

    user class add

---
+++ user.py
@@ -0,0 +1,14 @@
+class User:
+  
+  """
+    class for register user have to firstname, lastname, phonenumber, emaol, password, confirm password
+  """
+  def __init__(self, first_name, last_name, phone_number, email, password, con_pass):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.phone_number = phone_number
+    self.email = email
+    self.password = password
+    self.con_pass = con_pass
+
+  

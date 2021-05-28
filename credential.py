1f3b222858df3125d21f6f4b4b879d8b82de0b8c
layersony <sammaingi5@gmail.com>
2021-05-28 23:38:53 +0300

    credential class init

---
+++ credential.py
@@ -0,0 +1,13 @@
+class Credential:
+
+
+  mycred = []
+
+  def __init__(self, appType, appName, appUrl, appLogin, appPassword):
+    self.appType = appType
+    self.appName = appName
+    self.appUrl = appUrl
+    self.appLogin = appLogin
+    self.appPassword
+
+  

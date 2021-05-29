e797a4d2b41898ed0f1fc867bd27e930da8f57ce
layersony <sammaingi5@gmail.com>
2021-05-29 08:02:00 +0300

    save user detail registe

--- main.py
+++ main.py
@@ -10,6 +10,31 @@ def login():
     print("User exist")
   else:
     print("Username and password are Invalid")
+    reg = input("WOuld you like to register? (y/n)").lower()
+    if reg == "y":
+      register()
+    elif reg == "n":
+      login()
+    else:
+      print("Didn't Catch what you saying")
+      login()
+      
+
+def register():
+  usrname = input("Username (for login)")
+  User.saveUser(usrname + '|')
+  first_name = input("Firstname: ")
+  User.saveUser( first_name +'|' )
+  last_name = input("Last_name: ")
+  User.saveUser( last_name +'|' )
+  phone_number = input("Phone Number: ")
+  User.saveUser( phone_number +'|' )
+  email = input("Email: ")
+  User.saveUser( email +'|' )
+  password = input("password: ")
+  User.saveUser( password +'|' )
+  conf_password = input("Confirm Password: ")
+  User.saveUser( conf_password +'|' )
 
 
 
--- user.py
+++ user.py
@@ -1,11 +1,12 @@
 class User:
 
-  userdetail = ["Samuel", "Maingi", "0796727706", "sm@gmail.com", "123456", '123456']
+  userdetail = ["layersony","Samuel", "Maingi", "0796727706", "sm@gmail.com", "123456", '123456']
 
   """
     class for register user have to firstname, lastname, phonenumber, emaol, password, confirm password
   """
-  def __init__(self, first_name, last_name, phone_number, email, password, con_pass):
+  def __init__(self, username ,first_name, last_name, phone_number, email, password, con_pass):
+    self.username = username
     self.first_name = first_name
     self.last_name = last_name
     self.phone_number = phone_number
@@ -14,8 +15,8 @@ class User:
     self.con_pass = con_pass
 
   # save user data
-  def saveUser(self): # if new
-    User.userdetail.append(self)
+  # def saveUser(self): # if new
+  #   User.userdetail.append(self)
 
   
   def displayUser(self): # display user information from registration
@@ -23,7 +24,12 @@ class User:
   
   @classmethod
   def checkUserExist(cls, usrname, password):
-    if usrname == cls.userdetail[0] and password == cls.userdetail[5]:
+    
+    if usrname == cls.userdetail[0] and password == cls.userdetail[-1]:
       return True
     else:
       return False
+
+  def saveUser(inputtype):
+    with open("userlocker.txt", "w") as handle:
+      handle.write(inputtype)

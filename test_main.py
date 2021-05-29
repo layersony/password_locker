9963c55b34b1950b43d69e902cb84fbe1afcf1b9
layersony <sammaingi5@gmail.com>
2021-05-29 21:49:27 +0300

    docstring add to methods in test unit file

--- test_main.py
+++ test_main.py
@@ -4,16 +4,41 @@ from user import User
 from credential import Credential
 
 class Test_User(unittest.TestCase):
+  '''
+    tests below test the functionality of the class behaivours
 
+    Args:
+      unittest.TestCase: TestCase class that helps in creating test cases
+  '''
   def setUp(self):
-    self.usrname = 'layersony'
+    '''
+    Set up method to run before each test cases in the test User Class
+    '''
+    self.usrname = 'smaingi'
+    self.first_name = 'samuel'
+    self.last_name = 'maingi'
+    self.phone_number = '072345678'
+    self.email = 'sm@gmail.com'
     self.password = '12345'
+    self.con_pass = '12345'
     self.full = self.usrname + '|' + self.password
 
-  def tearDown(self):
-    pass
+  def test_init(self):
+    '''
+    test init test case if the object is initialized correctly
+    '''
+    self.assertEqual(self.usrname, "smaingi")
+    self.assertEqual(self.first_name, 'samuel')
+    self.assertEqual(self.last_name, 'maingi')
+    self.assertEqual(self.phone_number, '072345678')
+    self.assertEqual(self.email, 'sm@gmail.com')
+    self.assertEqual(self.con_pass, '12345')
+    self.assertEqual(self.password, "12345")
 
   def test_checkUser(self):
+    '''
+      test checkuser is for authentication to see if the user exists or not
+    '''
     User.saveUser(self.usrname + '|', 'a')
     User.saveUser(self.password, 'a')
 
@@ -28,6 +53,9 @@ class Test_User(unittest.TestCase):
       handle.write("")
   
   def test_save(self):
+    '''
+      test save to check it save function in user is working correctly
+    '''
     User.saveUser(self.usrname + '|', 'a')
     User.saveUser(self.password, 'a')
 
@@ -37,7 +65,16 @@ class Test_User(unittest.TestCase):
 
 
 class Test_Credential(unittest.TestCase):
+  '''
+    tests below test the functionality of the class behaivours
+
+    Args:
+      unittest.TestCase: TestCase class that helps in creating test cases
+  '''
   def setUp(self):
+    '''
+      Set up method to run before each test cases.
+    '''
     self.appType = 'twitter'
     self.appLogin = 'maingi'
     self.appUrl = 'twitter.com'
@@ -47,10 +84,16 @@ class Test_Credential(unittest.TestCase):
     self.full = 'twitter|maingi|twitter.com|sm@gmail.com|12345'
 
   def tearDown(self):
+    '''
+      tear down method to run after each test cases.
+    '''
     with open("credlocker.txt", "w") as handle:
       handle.write("")
 
   def test_save(self):
+    '''
+      test save to check it save function in credential is working correctly
+    '''
     Credential.saveUser(self.appType + '|')
     Credential.saveUser(self.appLogin + '|')
     Credential.saveUser(self.appUrl + '|')
@@ -69,6 +112,10 @@ class Test_Credential(unittest.TestCase):
     self.assertEqual(len(Credential.gen_pass()), 20)
 
   def test_find(self):
+    '''
+      method that tests if data saved from test unit is the same as the one found when 
+      run from the credential.py
+    '''
     Credential.saveUser(self.full)
     data = Credential.displayCred()
     found = Credential.findCred('maingi')
@@ -76,11 +123,18 @@ class Test_Credential(unittest.TestCase):
     self.assertEqual(data, found2)
 
   def test_display(self):
+    '''
+      method that tests if the same data that will be displayed in for the user will be the same as one produced here
+    '''
     Credential.saveUser(self.full)
     data = Credential.displayCred()
     self.assertEqual(data, Credential.displayCred())
     
   def test_delete(self):
+    '''
+      method that tests the number of items saved when the one is deleted
+    '''
+
     full = 'twitter|maingi|twitter.com|sm@gmail.com|12345\n'
     full2 = 'instagram|mutunga|instagram.com|tyu@gmail.com|87648976\n'
     full3 = 'facebook|layersony|facebook.com|unb@gmail.com|34786855\n'
